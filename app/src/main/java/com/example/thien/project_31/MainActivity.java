16e85317dcf4dec4af7f93796683c8067f057af1
Thien4512 <thien4512@gmail.com>
2018-07-27 15:09:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/thien/project_31/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.thien.project_31;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button_1, button_2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button_1 = (Button) findViewById(R.id.button_1);
+        button_2 = (Button) findViewById(R.id.button_2);
+
+        button_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MyService.class);
+                intent.putExtra("INIT_DATA",my_javaclass.name);
+                startService(intent);
+            }
+        });
+
+        button_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this,MyService.class));
+            }
+        });
+    }
+}
